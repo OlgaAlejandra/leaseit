52dc839eba654f0d3b7c52e3620436342ecdc9b9
Kuro <juliojtp4598@gmail.com>
2022-11-22 17:44:12 -0500

    Arrendador needs Fix

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -6,7 +6,7 @@ import { ArrendadorService } from './../../services/arrendador.service';
 import { ActivoService } from './../../services/activo.service';
 import { Activo } from './../../models/activo';
 import { Arrendador } from './../../models/arrendador';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
@@ -22,7 +22,7 @@ export class InputComponent implements OnInit {
   activos: Activo[]=[];
   arrendadores: Arrendador[]=[];
   activo?: Activo;
-  arrendador!: Arrendador;
+  arrendador?: Arrendador;
   idUser: any;
   constructor(
     private fb: FormBuilder,
@@ -56,8 +56,6 @@ export class InputComponent implements OnInit {
   }
   reactiveForm(){
     this.myForm=this.fb.group({
-      id:[''],
-      usuario:['',Validators.required],
       activo: ['', Validators.required],
       arrendador:['', Validators.required],
       moneda:['', Validators.required],
@@ -75,24 +73,47 @@ export class InputComponent implements OnInit {
     })
   }
   fillActivo(){
-    var temporal = [(this.myForm.value)];
+    var temporal = [(this.myForm.get('activo')?.value)];
     console.log(temporal)
-    this.activosService.getActivoId("1").subscribe((data: Activo)=>{
+    this.activosService.getActivoId(this.myForm.get('activo')?.value).subscribe((data: Activo)=>{
       this.myForm = this.fb.group({
-        prestamo:[data.precio,Validators.required]
-      })
-      //console.log(data);
-    })
-    console.log(temporal)
-
-    /* this.myForm=this.fb.group({
-      prestamo:[,Validators.required]  
-    }); */
-  }
+        activo: [data.id, Validators.required],
+        arrendador:[this.myForm.get('arrendador')?.value, [Validators.required]],
+        moneda:[this.myForm.get('moneda')?.value,[Validators.required]],
+        prestamo:[data.precio,Validators.required],
+        tasa:[this.myForm.get('tasa')?.value,[Validators.required]],
+        tiempo:[this.myForm.get('tiempo')?.value,[Validators.required]],
+        pgp_flag:[this.myForm.get('pgp_flag')?.value,[Validators.required]],
+        pgp_time:[this.myForm.get('pgp_time')?.value,[Validators.required]],
+        pgt_flag:[this.myForm.get('pgt_flag')?.value,[Validators.required]],
+        pgt_time:[this.myForm.get('pgt_time')?.value,[Validators.required]],
+        n_periodos:[this.myForm.get('n_periodos')?.value,[Validators.required]],
+        tasa_type:[this.myForm.get('tasa_type')?.value,[Validators.required]],
+        t_tasa:[this.myForm.get('t_tasa')?.value,[Validators.required]],
+      })})
+    
+    console.log(this.myForm.value)
+  } 
   fillArrendador(){
-    this.myForm=this.fb.group({
-      tasa:[this.arrendadores[(this.myForm.get('arrendador')!.value)].TEP,Validators.required]
-  })
+    this.arrendadoresService.getArrendadorId(this.myForm.get('arrendador')?.value).subscribe((data: Arrendador)=>{
+      this.myForm = this.fb.group({
+        activo: [this.myForm.get('activo')?.value, [Validators.required]],
+        arrendador:[data.id, [Validators.required]],
+        moneda:[this.myForm.get('moneda')?.value, [Validators.required]],
+        prestamo:[this.myForm.get('prestamo')?.value,[Validators.required]],
+        tasa:[data.TEP,[Validators.required]],
+        tiempo:[this.myForm.get('tiempo')?.value,[Validators.required]],
+        pgp_flag:[this.myForm.get('pgp_flag')?.value,[Validators.required]],
+        pgp_time:[this.myForm.get('pgp_time')?.value,[Validators.required]],
+        pgt_flag:[this.myForm.get('pgt_flag')?.value,[Validators.required]],
+        pgt_time:[this.myForm.get('pgt_time')?.value,[Validators.required]],
+        n_periodos:[this.myForm.get('n_periodos')?.value,[Validators.required]],
+        tasa_type:[data.tasa_type,,Validators.required],
+        t_tasa:[data.t_tasa,,Validators.required],
+      })})
+      console.log(this.myForm.value)
   }
-
+  changeClient(value: any) {
+    console.log(value);
+}
 }
