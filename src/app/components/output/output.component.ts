84bdea9847cbd3a4d6ef5f10e66e3cb3886aa29b
Kuro <juliojtp4598@gmail.com>
2022-11-24 16:15:35 -0500

    Table Show

--- src/app/components/output/output.component.ts
+++ src/app/components/output/output.component.ts
@@ -1,3 +1,4 @@
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { OperationService } from 'src/app/services/operation.service';
 import { Arrendador } from './../../models/arrendador';
 import { Activo } from './../../models/activo';
@@ -37,17 +38,27 @@ const ELEMENT_DATA: Output[]=[
   styleUrls: ['./output.component.css']
 })
 export class OutputComponent implements OnInit {
-  displayedColumns: string[] = ['n°', 'P.G', 'Saldo Inicial','Intereses','Cuota','Amortizacion',
+ /*  displayedColumns: string[] = ['n°', 'P.G', 'Saldo Inicial','Intereses','Cuota','Amortizacion',
 'Seguro Riesgo','Recompra','Saldo Final','Depreciacion','Ahorro Tributario','IGV','Flujo Bruto',
-'Flujo con IGV','Flujo Neto'];
+'Flujo con IGV','Flujo Neto']; */
+  displayedColumns: string[] = ['periodo','pg','saldo_i','interes','cuota',
+'amortizacion','s_riesgo','comision','recompra','saldo_f','depreciacion',
+'a_tributario','IGV','f_bruto','f_bruto_igv','flujo_neto']
   dataSource = new MatTableDataSource<Output>();
-  output: Output[]=[];
+  outputs: Output[]=[];
   operation!: Operation;
+  temp!: Output;
+  activo!: Activo;
+  arrendador!: Arrendador;
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
   constructor(private operationService: OperationService) { }
   ngOnInit(): void {
     this.EntryData();   
   }
   EntryData(){
+    this.fillOutput()
+    console.log(this.temp)
     this.operationService.getOperationId(1).subscribe((data: any) => {
       this.operation=data;
     },(error: any) => {
@@ -55,27 +66,48 @@ export class OutputComponent implements OnInit {
     });
   }
   fillTable(input: Operation){
-    var temp!: Output;
-    for (let i = 0; i <= ((this.operation.tiempo_o*360))/this.operation.frecuencia; i++) {
-        temp.periodo=i;
+    for (let i = 0; i <=((this.operation.tiempo_o*360)/this.operation.frecuencia); i++) {
+        this.temp.periodo=i;
         if(this.operation.pgp_flag==false && this.operation.pgt_flag ==false){
-          temp.pg='S'}
+          this.temp.pg='S'}
         else if(this.operation.pgp_flag==true){
-          temp.pg='P'
+          this.temp.pg='P'
         }
         else if(this.operation.pgt_flag==true){
-          temp.pg='T'
+          this.temp.pg='T'
         }
-        temp.saldo_i=0
-        temp.cuota=0
-        
 
+        /*if(i=0){
+          this.temp.periodo=0;
+          this.temp.saldo_i=0;
+          this.temp.cuota=0;
+          this.temp.amortizacion=0;
+          this.temp.s_riesgo=0;
+          this.temp.comision=0;
+          this.temp.recompra=0;
+          this.temp.saldo_f=this.operation.activo.precio-this.operation.activo.g_inicial;
+          this.temp.depreciacion=0;
+          this.temp.a_tributario=0;
+          this.temp.IGV=0;
+          this.temp.f_bruto=this.temp.saldo_f;
+          this.temp.f_bruto_igv=this.temp.saldo_f;
+          this.temp.flujo_neto=this.temp.saldo_f;
+        }        */
+        console.log(this.temp)
+        this.outputs.push(this.temp)
+        console.log(this.outputs)
     }
+    this.dataSource = new MatTableDataSource<Output>(this.outputs)
+    this.dataSource.paginator = this.paginator;
+
   }
   test(){
     console.log(this.operation);
     console.log(((this.operation.tiempo_o*360))/this.operation.frecuencia)
+    this.fillTable(this.operation)
+    
   }
-
-
+  fillOutput(){
+    this.temp= new Output(1,"1",1,1,1,1,1,1,1,1,1,1,1,1,1,1);
+  }
 }
--- src/app/models/output.ts
+++ src/app/models/output.ts
@@ -1,4 +1,7 @@
-export interface Output{
+import { never } from "rxjs";
+
+
+export class Output{
     periodo: number;
     pg:string;
     saldo_i:number;
@@ -15,4 +18,76 @@ export interface Output{
     f_bruto:number;
     f_bruto_igv:number;
     flujo_neto:number;
+
+    constructor(a:number,b:string,c:number,d:number,e:number,f:number,g:number,
+        h:number,i:number,j:number,k:number,l:number,m:number,n:number,o:number,p:number,){
+            this.periodo=a;
+            this.pg=b;
+            this.saldo_i=c;
+            this.interes=d;
+            this.cuota=e;
+            this.amortizacion=f;
+            this.s_riesgo=g;
+            this.comision=h;
+            this.recompra=i;
+            this.saldo_f=j;
+            this.depreciacion=k;
+            this.a_tributario=l;
+            this.IGV=m;
+            this.f_bruto=n;
+            this.f_bruto_igv=o;
+            this.flujo_neto=p;
+    };
+    
+    setPeriodo(p: number){
+        this.periodo=p
+    }
+    setPG(p: string){
+        this.pg=p
+    }
+    setSaldo_i(p: number){
+        this.saldo_i=p
+    }
+    setInteres(p: number){
+        this.interes=p
+    }
+    setCuota(p: number){
+        this.cuota=p
+    }
+    setAmortizacion(p: number){
+        this.amortizacion=p
+    }
+    setS_riesgo(p: number){
+        this.s_riesgo=p
+    }
+    setComision(p: number){
+        this.comision=p
+    }
+    setRecompra(p: number){
+        this.recompra=p
+    }
+    setSaldo_f(p: number){
+        this.saldo_f=p
+    }
+    setDepreciacion(p: number){
+        this.depreciacion=p
+    }
+    setA_tributario(p: number){
+        this.a_tributario=p
+    }
+    setIGV(p: number){
+        this.IGV=p
+    }
+    setF_bruto(p: number){
+        this.f_bruto=p
+    }
+    setF_bruto_igv(p: number){
+        this.f_bruto_igv=p
+    }
+    setF_neto(p: number){
+        this.flujo_neto=p
+    }
+
 }
+
+   
